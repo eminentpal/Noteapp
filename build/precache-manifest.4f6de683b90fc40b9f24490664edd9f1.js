6cc77e7aec968c653eb3e97264fc04bbdd3ef02a
eminentpal <eziechukwudi@gmail.com>
2021-03-09 22:06:53 +0100

    changes in jason

---
+++ build/precache-manifest.4f6de683b90fc40b9f24490664edd9f1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db0b798ef368756f932142ca6e8dcb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a7709dd9262f9ac0407",
+    "url": "/static/js/2.40537b90.chunk.js"
+  },
+  {
+    "revision": "fb9fe99f9f472ddd76ab",
+    "url": "/static/js/main.5a2483d2.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
